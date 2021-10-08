f43324d3ab268cb1b2892ed80d53177c8e2ef116
Ian Duncan <ian@mercury.com>
2021-10-08 15:00:30 +0200

    Add support for links and events to honeycomb exporter

--- exporters/honeycomb/src/OpenTelemetry/Exporters/Honeycomb.hs
+++ exporters/honeycomb/src/OpenTelemetry/Exporters/Honeycomb.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NumericUnderscores #-}
-module OpenTelemetry.Exporters.Honeycomb 
+module OpenTelemetry.Exporters.Honeycomb
   ( makeHoneycombExporter
   , Config(..)
   , config
@@ -15,7 +15,7 @@ import qualified Data.HashMap.Strict as H
 import Data.Text (Text)
 import Honeycomb
 import OpenTelemetry.Trace.SpanExporter
-import OpenTelemetry.Trace (ImmutableSpan (..), SpanContext (spanId, traceId), getSpanContext)
+import OpenTelemetry.Trace (ImmutableSpan (..), SpanContext (spanId, traceId), getSpanContext, Event (..), Link(..))
 import OpenTelemetry.Resource
 import OpenTelemetry.Trace.Id (Base(Base16), spanIdBaseEncodedText, traceIdBaseEncodedText)
 import qualified Torsor
@@ -44,30 +44,57 @@ instance ToJSON HoneycombFormattedAttribute where
   toJSON (HoneycombFormattedAttribute (AttributeValue a)) = primitiveAttributeToJSON a
   toJSON (HoneycombFormattedAttribute (AttributeArray a)) = toJSON $ fmap primitiveAttributeToJSON a
 
-makeEvents :: HoneycombClient -> Text -> ImmutableSpan -> [Event]
-makeEvents client svcName ImmutableSpan{..} = 
+makeEvents :: HoneycombClient -> Text -> ImmutableSpan -> [Honeycomb.Event]
+makeEvents client svcName ImmutableSpan{..} =
   concat [[spanEvent], eventEvents, linkEvents]
   where
+    spanTime = Just $ clockTimeToChronosTime spanStart
     spanEvent = event
       { fields = fields
-      , timestamp = Just $ clockTimeToChronosTime spanStart
+      , timestamp = spanTime
       }
-    attrFields = H.fromList $ map (\(k, v) -> (k, toJSON $ HoneycombFormattedAttribute v)) spanAttributes
+    attrFields = H.fromList . map (\(k, v) -> (k, toJSON $ HoneycombFormattedAttribute v))
     unsafeParentId = case spanParent of
-      Nothing -> Nothing 
+      Nothing -> Nothing
       Just s -> Just $ spanId $ unsafeDupablePerformIO (getSpanContext s :: IO SpanContext)
 
-    fields = 
-      H.insert "name" (toJSON spanName) $ 
-      H.insert "service_name" (toJSON svcName) $
-      H.insert "trace.span_id" (toJSON $ spanIdBaseEncodedText Base16 $ spanId spanContext) $
-      H.insert "trace.parent_id" (toJSON (spanIdBaseEncodedText Base16 <$> unsafeParentId)) $
-      H.insert "trace.trace_id" (toJSON $ traceIdBaseEncodedText Base16 $ traceId spanContext) $
-      H.insert "duration_ms" (toJSON $ durationMs spanStart spanEnd) $
-      attrFields
+    encodedSpanId = spanIdBaseEncodedText Base16 $ spanId spanContext
+    encodedTraceId = traceIdBaseEncodedText Base16 $ traceId spanContext
 
-    eventEvents = []
-    linkEvents = []
+    fields =
+      H.insert "name" (toJSON spanName) $
+      H.insert "service_name" (toJSON svcName) $
+      H.insert "trace.span_id" (toJSON encodedSpanId) $
+      H.insert "trace.parent_id" (toJSON (spanIdBaseEncodedText Base16 <$> unsafeParentId)) $
+      H.insert "trace.trace_id" (toJSON encodedTraceId) $
+      H.insert "duration_ms" (toJSON $ durationMs spanStart spanEnd) $
+      attrFields spanAttributes
+
+    eventEvents = map eventToEvent spanEvents
+      where
+        eventToEvent e = event
+          { timestamp = Just $ clockTimeToChronosTime (eventTimestamp e)
+          , fields = 
+              H.insert "name" (toJSON $ eventName e) $
+              H.insert "service_name" (toJSON svcName) $
+              H.insert "meta.annotation_type" (String "span_event") $
+              H.insert "trace.parent_id" (toJSON encodedSpanId) $
+              H.insert "trace.trace_id" (toJSON encodedTraceId) $
+              attrFields (eventAttributes e)
+          }
+
+    linkEvents = map linkToEvent spanLinks
+      where
+        linkToEvent l = event
+          { timestamp = spanTime 
+          , fields =
+              H.insert "meta.annotation_type" (String "link") $
+              H.insert "trace.parent_id" (toJSON encodedSpanId) $
+              H.insert "trace.trace_id" (toJSON encodedTraceId) $
+              H.insert "trace.link.span_id" (toJSON $ spanIdBaseEncodedText Base16 $ spanId $ linkContext l) $
+              H.insert "trace.link.trace_id" (toJSON $ traceIdBaseEncodedText Base16 $ traceId $ linkContext l) $
+              attrFields (linkAttributes l)
+          }
 
 clockTimeToChronosTime :: TimeSpec -> Time
 clockTimeToChronosTime TimeSpec{..} = Time ((sec * 1_000_000_000) + nsec)
