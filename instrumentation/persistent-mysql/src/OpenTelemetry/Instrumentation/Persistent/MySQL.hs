4c84f4f729fc7bd9897816ddf6c3b2c30d625b48
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2023-02-10 15:23:24 +0900

    persistent-mysql attributes

--- instrumentation/persistent-mysql/src/OpenTelemetry/Instrumentation/Persistent/MySQL.hs
+++ instrumentation/persistent-mysql/src/OpenTelemetry/Instrumentation/Persistent/MySQL.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 -- option for module re-export
 {-# OPTIONS_GHC -Wno-missing-import-lists #-}
 
@@ -8,9 +11,9 @@ module OpenTelemetry.Instrumentation.Persistent.MySQL (
   createMySQLPool,
   module Database.Persist.Sql,
   MySQL.ConnectInfo (..),
-  MySQLBase.SSLInfo (..),
+  MySQL.SSLInfo (..),
   MySQL.defaultConnectInfo,
-  MySQLBase.defaultSSLInfo,
+  MySQL.defaultSSLInfo,
   Orig.MySQLConf (..),
 
   -- * @ON DUPLICATE KEY UPDATE@ Functionality
@@ -26,14 +29,21 @@ module OpenTelemetry.Instrumentation.Persistent.MySQL (
 
 import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Control.Monad.Logger (MonadLoggerIO)
+import Data.Foldable (Foldable (fold))
+import Data.Functor ((<&>))
+import Data.IP (IP)
+import Data.Maybe (fromMaybe)
+import Data.Monoid (Last (Last, getLast))
 import Data.Pool (Pool)
+import Data.String (IsString (fromString))
 import Data.Text (Text)
-import qualified Database.MySQL.Base as MySQLBase
-import qualified Database.MySQL.Simple as MySQL
+import Database.MySQL.Base (ConnectInfo (..))
+import qualified Database.MySQL.Base as MySQL
 import qualified Database.Persist.MySQL as Orig
 import Database.Persist.Sql
 import qualified OpenTelemetry.Instrumentation.Persistent as Otel
 import qualified OpenTelemetry.Trace.Core as Otel
+import Text.Read (readMaybe)
 
 
 {- | Create a MySQL connection pool.  Note that it's your
@@ -43,14 +53,14 @@ unneeded.  Use 'withMySQLPool' for automatic resource control.
 createMySQLPool ::
   (MonadUnliftIO m, MonadLoggerIO m) =>
   Otel.TracerProvider ->
-  -- | Attributes that are specific to providers like MySQL, PostgreSQL, etc.
+  -- | Additional attributes.
   [(Text, Otel.Attribute)] ->
   -- | Connection information.
   MySQL.ConnectInfo ->
   -- | Number of connections to be kept open in the pool.
   Int ->
   m (Pool SqlBackend)
-createMySQLPool tp attrs ci = createSqlPool $ open' tp attrs ci
+createMySQLPool tp attrs ci = createSqlPool $ fmap snd . openMySQLConn tp attrs ci
 
 
 {- | Create a MySQL connection pool and run the given action.
@@ -61,7 +71,7 @@ outside the action since it may be already been released.
 withMySQLPool ::
   (MonadLoggerIO m, MonadUnliftIO m) =>
   Otel.TracerProvider ->
-  -- | Attributes that are specific to providers like MySQL, PostgreSQL, etc.
+  -- | Additional attributes.
   [(Text, Otel.Attribute)] ->
   -- | Connection information.
   MySQL.ConnectInfo ->
@@ -70,22 +80,49 @@ withMySQLPool ::
   -- | Action to be executed that uses the connection pool.
   (Pool SqlBackend -> m a) ->
   m a
-withMySQLPool tp attrs ci = withSqlPool $ open' tp attrs ci
+withMySQLPool tp attrs ci = withSqlPool $ fmap snd . openMySQLConn tp attrs ci
 
 
 {- | Open a connection to MySQL server, initialize the 'SqlBackend' and return
 their tuple
+
+About attributes, see https://opentelemetry.io/docs/reference/specification/trace/semantic_conventions/database/.
 -}
 openMySQLConn ::
   Otel.TracerProvider ->
-  -- | Attributes that are specific to providers like MySQL, PostgreSQL, etc.
+  -- | Additional attributes.
   [(Text, Otel.Attribute)] ->
+  -- | Connection information.
   MySQL.ConnectInfo ->
   LogFunc ->
   IO (MySQL.Connection, SqlBackend)
-openMySQLConn tp attrs ci logFunc = do
+openMySQLConn tp attrs ci@MySQL.ConnectInfo {connectUser, connectPort, connectOptions, connectHost} logFunc = do
+  let
+    portAttr, transportAttr :: Otel.Attribute
+    portAttr = fromString $ show connectPort
+    transportAttr =
+      fromMaybe "ip_tcp" $
+        getLast $
+          fold $
+            connectOptions <&> \case
+              MySQL.Protocol p ->
+                Last $ Just $ case p of
+                  MySQL.TCP -> "ip_tcp"
+                  MySQL.Socket -> "other"
+                  MySQL.Pipe -> "pipe"
+                  MySQL.Memory -> "inproc"
+              _ -> Last Nothing
+    -- "net.sock.family" is unnecessary because it must be "inet" when "net.sock.peer.addr" or "net.sock.host.addr" is set.
+    attrs' =
+      ("db.connection_string", fromString $ showsPrecConnectInfoMasked 0 ci "")
+        : ("db.user", fromString connectUser)
+        : ("net.peer.port", portAttr)
+        : ("net.sock.peer.port", portAttr)
+        : ("net.transport", transportAttr)
+        : (maybe "net.peer.name" (const "net.sock.peer.addr") (readMaybe connectHost :: Maybe IP), fromString connectHost)
+        : attrs
   (conn, backend) <- Orig.openMySQLConn ci logFunc
-  backend' <- Otel.wrapSqlBackend' tp attrs backend
+  backend' <- Otel.wrapSqlBackend' tp attrs' backend
   pure (conn, backend')
 
 
@@ -95,16 +132,39 @@ of connections, only one connection is opened.
 withMySQLConn ::
   (MonadUnliftIO m, MonadLoggerIO m) =>
   Otel.TracerProvider ->
-  -- | Attributes that are specific to providers like MySQL, PostgreSQL, etc.
+  -- | Additional attributes.
   [(Text, Otel.Attribute)] ->
   -- | Connection information.
   MySQL.ConnectInfo ->
   -- | Action to be executed that uses the connection.
   (SqlBackend -> m a) ->
   m a
-withMySQLConn tp attrs ci = withSqlConn $ open' tp attrs ci
+withMySQLConn tp attrs ci = withSqlConn $ fmap snd . openMySQLConn tp attrs ci
 
 
--- | Internal function that opens a connection to the MySQL server.
-open' :: Otel.TracerProvider -> [(Text, Otel.Attribute)] -> MySQL.ConnectInfo -> LogFunc -> IO SqlBackend
-open' tp attrs ci logFunc = snd <$> openMySQLConn tp attrs ci logFunc
+showsPrecConnectInfoMasked :: Int -> MySQL.ConnectInfo -> ShowS
+showsPrecConnectInfoMasked d MySQL.ConnectInfo {connectHost, connectPort, connectUser, connectDatabase, connectOptions, connectPath, connectSSL} =
+  showParen (d > 10) $
+    showString "ConnectInfo {"
+      . showString "connectHost = "
+      . shows connectHost
+      . showString ", "
+      . showString "connectPort = "
+      . shows connectPort
+      . showString ", "
+      . showString "connectUser = "
+      . shows connectUser
+      . showString ", "
+      . showString "connectPassword = \"****\", "
+      . showString "connectDatabase = "
+      . shows connectDatabase
+      . showString ", "
+      . showString "connectOptions = "
+      . shows connectOptions
+      . showString ", "
+      . showString "connectPath = "
+      . shows connectPath
+      . showString ", "
+      . showString "connectSSL = "
+      . shows connectSSL
+      . showString "}"
