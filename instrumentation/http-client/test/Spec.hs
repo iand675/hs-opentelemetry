6b1baaae85f8f03f0d52a5100c9e5ccdd5db2818
evanlauer1 <evanlauer@mercury.com>
2024-05-21 15:57:45 -0700

    WIP testing for http-client

--- instrumentation/http-client/test/Spec.hs
+++ instrumentation/http-client/test/Spec.hs
@@ -1,3 +1,19 @@
+import Network.HTTP.Client (Request (requestHeaders), defaultRequest)
+import qualified OpenTelemetry.Context as Context
+import OpenTelemetry.Instrumentation.HttpClient.Raw (httpTracerProvider, instrumentRequest)
+import OpenTelemetry.Propagator
+import OpenTelemetry.Trace.Core (getTracerProviderPropagators, getTracerTracerProvider)
+import Test.Hspec
+
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "OpenTelemetry.Instrumentation.HttpClient" $ do
+    describe "Raw" $ do
+      describe "instrumentRequest" $ do
+        specify "Request has the correct attributes" $ do
+          request <- instrumentRequest mempty Context.empty defaultRequest
+          tracer <- httpTracerProvider
+          context <- extract (getTracerProviderPropagators $ getTracerTracerProvider $ tracer) (requestHeaders request) Context.empty
+          let maybeAttributes = Context.lookupSpan context
+          pending
