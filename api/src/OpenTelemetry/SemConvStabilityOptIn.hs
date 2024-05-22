8a11e96cd7315613323b7f823ce20bb624c4f4b1
evanlauer1 <evanlauer@mercury.com>
2024-05-22 10:22:13 -0700

    Updated getSemConvStabilityOptIn  so it parses environment variable as a comma-separated list

--- api/src/OpenTelemetry/SemConvStabilityOptIn.hs
+++ api/src/OpenTelemetry/SemConvStabilityOptIn.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module OpenTelemetry.SemConvStabilityOptIn (
   getSemConvStabilityOptIn,
   SemConvStabilityOptIn (Stable, Both, Old),
 ) where
 
+import qualified Data.Text as T
 import System.Environment (lookupEnv)
 
 
@@ -12,10 +13,15 @@ data SemConvStabilityOptIn = Stable | Both | Old
 
 
 getSemConvStabilityOptIn :: IO SemConvStabilityOptIn
-getSemConvStabilityOptIn =
-  ( \case
-      Just "http" -> Stable
-      Just "http/dup" -> Both
-      _ -> Old
-  )
-    <$> lookupEnv "OTEL_SEMCONV_STABILITY_OPT_IN"
+getSemConvStabilityOptIn = do
+  menv <- lookupEnv "OTEL_SEMCONV_STABILITY_OPT_IN"
+  let menvs = fmap T.strip . T.splitOn "," . T.pack <$> menv
+  pure $ case menvs of
+    Nothing -> Old
+    Just envs ->
+      if "http/dup" `elem` envs
+        then Both
+        else
+          if "http" `elem` envs
+            then Stable
+            else Old
