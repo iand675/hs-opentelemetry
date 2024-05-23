266b974fe886a5adafe6d3fcf25ac878ab5c8add
evanlauer1 <evanlauer@mercury.com>
2024-05-22 17:06:14 -0700

    Rewrote tests to eliminate boilerplate and increase readability

--- api/test/OpenTelemetry/SemConvStabilityOptInSpec.hs
+++ api/test/OpenTelemetry/SemConvStabilityOptInSpec.hs
@@ -16,27 +16,17 @@ spec = do
       unsetEnv envVarName
       semConvStabilityOptIn <- getSemConvStabilityOptIn
       semConvStabilityOptIn `shouldBe` Old
-    it "defaults to 'Old' when env var has incorrect value" $ do
-      setEnv envVarName "http/du" -- intentionally similar to "http/dup" and "http"
-      semConvStabilityOptIn <- getSemConvStabilityOptIn
-      semConvStabilityOptIn `shouldBe` Old
-    it "is 'Stable' when env var is 'http'" $ do
-      setEnv envVarName "http"
-      semConvStabilityOptIn <- getSemConvStabilityOptIn
-      semConvStabilityOptIn `shouldBe` Stable
-    it "is 'Both' when env var is 'http/dup'" $ do
-      setEnv envVarName "http/dup"
-      semConvStabilityOptIn <- getSemConvStabilityOptIn
-      semConvStabilityOptIn `shouldBe` Both
-    it "is 'Both' when env var is 'http/dup,http'" $ do
-      setEnv envVarName "http/dup,http"
-      semConvStabilityOptIn <- getSemConvStabilityOptIn
-      semConvStabilityOptIn `shouldBe` Both
-    it "is 'Both' when env var is 'http,http/dup'" $ do
-      setEnv envVarName "http,http/dup"
-      semConvStabilityOptIn <- getSemConvStabilityOptIn
-      semConvStabilityOptIn `shouldBe` Both
-    it "is 'Both' when env var is 'http,something-random,http/dup'" $ do
-      setEnv envVarName "http,http/dup"
-      semConvStabilityOptIn <- getSemConvStabilityOptIn
-      semConvStabilityOptIn `shouldBe` Both
+    mapM_
+      ( \(envVarVal, expectedVal) ->
+          it ("returns '" ++ show expectedVal ++ "' when env var is '" ++ show envVarVal ++ "'") $ do
+            setEnv envVarName envVarVal
+            semConvStabilityOptIn <- getSemConvStabilityOptIn
+            semConvStabilityOptIn `shouldBe` expectedVal
+      )
+      [ ("http", Stable)
+      , ("http/du", Old) -- intentionally similar to both "http/dup" and "http"
+      , ("http/dup", Both)
+      , ("http/dup,http", Both)
+      , ("http,http/dup", Both)
+      , ("http,something-random,http/dup", Both)
+      ]
