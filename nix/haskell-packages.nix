babc706a4b843a70de09e22002980c7af0c38178
Ian Duncan <ian@iankduncan.com>
2024-05-29 20:20:43 +0200

    CI overhaul (#120)
    * test
    
    * Add some more GHC versions
    
    * Add pre-commit checks and some other goodies
    
    * missed some hooks there
    
    * Exclude shell builds for now
    
    * GHC 8.10 and 9.0 aren't in nixpkgs anymore?
    
    * Get particular about build packages
    
    * Don't torture garnix with too many packages
    
    * fix glob
    
    * Hardcode packages since globs are wimpy
    
    * pre-commit checks
    
    * turn off old checks
    
    * Get checks to agree with each other

---
+++ nix/haskell-packages.nix
@@ -0,0 +1,89 @@
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit
+    (builtins)
+    attrNames
+    listToAttrs
+    map
+    ;
+  inherit (import ./matrix.nix) supportedGHCVersions;
+in rec {
+  localPackages = {
+    hs-opentelemetry-api = ../api;
+    hs-opentelemetry-sdk = ../sdk;
+    hs-opentelemetry-otlp = ../otlp;
+    hs-opentelemetry-exporter-handle = ../exporters/handle;
+    hs-opentelemetry-exporter-in-memory = ../exporters/in-memory;
+    hs-opentelemetry-exporter-otlp = ../exporters/otlp;
+    hs-opentelemetry-propagator-b3 = ../propagators/b3;
+    hs-opentelemetry-propagator-datadog = ../propagators/datadog;
+    hs-opentelemetry-propagator-w3c = ../propagators/w3c;
+    hs-opentelemetry-instrumentation-cloudflare = ../instrumentation/cloudflare;
+    hs-opentelemetry-instrumentation-conduit = ../instrumentation/conduit;
+    hs-opentelemetry-instrumentation-hspec = ../instrumentation/hspec;
+    hs-opentelemetry-instrumentation-http-client = ../instrumentation/http-client;
+    hs-opentelemetry-instrumentation-persistent = ../instrumentation/persistent;
+    hs-opentelemetry-instrumentation-persistent-mysql = ../instrumentation/persistent-mysql;
+    hs-opentelemetry-instrumentation-postgresql-simple = ../instrumentation/postgresql-simple;
+    hs-opentelemetry-instrumentation-yesod = ../instrumentation/yesod;
+    hs-opentelemetry-instrumentation-wai = ../instrumentation/wai;
+  };
+
+  localPackageCabalDerivations = hfinal: lib.mapAttrs (name: path: hfinal.callCabal2nix name path {}) localPackages;
+
+  pluckLocalPackages = hpkgs: let
+    narrowAttrs = sourceAttrs: matchAttrs:
+      lib.foldl' (
+        acc: key:
+          if lib.hasAttr key matchAttrs
+          then acc // {"${key}" = sourceAttrs.${key};}
+          else acc
+      ) {} (lib.attrNames sourceAttrs);
+  in
+    narrowAttrs hpkgs localPackages;
+
+  extendedPackageSetByGHCVersions = listToAttrs (
+    map (ghcVersion: {
+      name = ghcVersion;
+      value = pkgs.haskell.packages.${ghcVersion}.extend (final: _prev: localPackageCabalDerivations final);
+    })
+    supportedGHCVersions
+  );
+
+  mapKeyValues = f: attrs: let
+    inherit (lib.attrsets) foldlAttrs;
+  in
+    foldlAttrs (acc: key: value: acc // f key value) {} attrs;
+
+  localPackageMatrix =
+    mapKeyValues (
+      key: value: let
+        myLocalPackages = pluckLocalPackages value;
+        k = "hs-opentelemetry-suite-${key}";
+      in {
+        "${k}" = pkgs.buildEnv {
+          name = k;
+          paths = lib.attrValues myLocalPackages;
+        };
+      }
+    )
+    extendedPackageSetByGHCVersions;
+
+  localDevPackageDeps = hsPackageSet:
+    lib.concatMapAttrs (_: v:
+      listToAttrs (
+        map (p: {
+          name = p.pname;
+          value = p;
+        })
+        v.getBuildInputs.haskellBuildInputs
+      ))
+    (pluckLocalPackages hsPackageSet);
+
+  localDevPackageDepsAsAttrSet = hsPackageSet:
+    lib.filterAttrs (k: _: !builtins.hasAttr k (pluckLocalPackages hsPackageSet))
+    (localDevPackageDeps hsPackageSet);
+}
