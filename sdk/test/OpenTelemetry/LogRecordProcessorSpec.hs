77a9b64ea145e24156c226a9d69511c7869c924d
evanlauer1 <evanlauer@mercury.com>
2024-07-09 17:05:42 -0700

    working on tests

--- sdk/test/OpenTelemetry/LogRecordProcessorSpec.hs
+++ sdk/test/OpenTelemetry/LogRecordProcessorSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module OpenTelemetry.LogRecordProcessorSpec where
 
@@ -6,6 +7,8 @@ import Data.IORef
 import qualified Data.Vector as V
 import OpenTelemetry.Exporter.LogRecord
 import OpenTelemetry.Internal.Common.Types
+import OpenTelemetry.LogRecordExporter
+import OpenTelemetry.LogRecordProcessor.Simple
 import OpenTelemetry.Logs.Core
 import Test.Hspec
 
@@ -47,9 +50,12 @@ spec = describe "LogRecordProcessor" $ do
   describe "Simple Processor" $ do
     it "Sends LogRecords to the Exporter" $ do
       (numExportsRef, testExporter) <- getTestExporter
+      processor <- simpleProcessor testExporter
 
-      let lp = createLoggerProvider [testExporter] emptyLoggerProviderOptions
-          l = makeLogger lp
+      let lp = createLoggerProvider [processor] emptyLoggerProviderOptions
+          l = makeLogger lp "Test Library"
+
+      emitLogRecord l (emptyLogRecordArguments "something")
 
       pending
 
