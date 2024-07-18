bca67201e5123b45e101ed6c2668b137de81524c
evanlauer1 <evanlauer@mercury.com>
2024-07-17 17:08:04 -0700

    Added pattern Synonyms for Exporters and Processors

--- api/src/OpenTelemetry/Exporter.hs
+++ api/src/OpenTelemetry/Exporter.hs
@@ -1,8 +1,24 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module OpenTelemetry.Exporter (
-  module OpenTelemetry.Exporter.Span,
+  Exporter,
+  SpanExporter (Exporter, exporterExport, exporterShutdown),
+  ExportResult (..),
 ) where
 
+import Data.HashMap.Strict (HashMap)
+import Data.Vector (Vector)
 import OpenTelemetry.Exporter.Span
+import OpenTelemetry.Internal.Common.Types (InstrumentationLibrary)
+import OpenTelemetry.Internal.Trace.Types (ImmutableSpan)
 
 
 type Exporter a = SpanExporter
+
+
+pattern Exporter :: (HashMap InstrumentationLibrary (Vector ImmutableSpan) -> IO ExportResult) -> IO () -> Exporter ImmutableSpan
+pattern Exporter {exporterExport, exporterShutdown} =
+  SpanExporter
+    { spanExporterExport = exporterExport
+    , spanExporterShutdown = exporterShutdown
+    }
--- api/src/OpenTelemetry/Processor.hs
+++ api/src/OpenTelemetry/Processor.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module OpenTelemetry.Processor (
-  module OpenTelemetry.Processor.Span,
+  Processor,
+  SpanProcessor (
+    Processor,
+    processorOnStart,
+    processorOnEnd,
+    processorShutdown,
+    processorForceFlush
+  ),
+  ShutdownResult (..),
 ) where
 
+import Control.Concurrent.Async (Async)
+import Data.IORef (IORef)
+import OpenTelemetry.Context (Context)
+import OpenTelemetry.Internal.Trace.Types (ImmutableSpan)
 import OpenTelemetry.Processor.Span
 
+
+type Processor = SpanProcessor
+
+
+pattern Processor
+  :: (IORef ImmutableSpan -> Context -> IO ())
+  -> (IORef ImmutableSpan -> IO ())
+  -> IO (Async ShutdownResult)
+  -> IO ()
+  -> SpanProcessor
+pattern Processor {processorOnStart, processorOnEnd, processorShutdown, processorForceFlush} =
+  SpanProcessor
+    { spanProcessorOnStart = processorOnStart
+    , spanProcessorOnEnd = processorOnEnd
+    , spanProcessorShutdown = processorShutdown
+    , spanProcessorForceFlush = processorForceFlush
+    }
