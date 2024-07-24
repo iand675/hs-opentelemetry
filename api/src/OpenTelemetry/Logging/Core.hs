c70625328cb31ed95661f574b35bb985d5586cb7
evanlauer1 <evanlauer@mercury.com>
2024-07-23 17:11:59 -0700

    Added Logging when creating spans or emitting logs drops attributes. Updated Severity Number (#133)
    * Added Logging to emitLogRecord and createSpanWithoutCallStack when dropping attributes. Updated Severity Number
    
    * Added comments warning users not to use emitLogRecord
    
    * Added further documentation to warn against using emitOTelLogRecord
    
    * Explicitly handle Unknown case in toShortName
    
    * Changed Err to Error in SeverityNumber
    
    * Moved Logging.Core to Internal.Logging.Core to hide functions
    
    * Revert "Moved Logging.Core to Internal.Logging.Core to hide functions"
    
    This reverts commit 32c5e58978aef92d94bc9042a471340daafd6002.
    
    * Moved Logging.Core to Internal.Logging.Core
    
    * Re-added Logging.Core with re-exported functions from Internal.Logging.Core

--- api/src/OpenTelemetry/Logging/Core.hs
+++ api/src/OpenTelemetry/Logging/Core.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE TypeApplications #-}
-
 module OpenTelemetry.Logging.Core (
   -- * @LoggerProvider@ operations
   LoggerProvider (..),
@@ -18,121 +15,12 @@ module OpenTelemetry.Logging.Core (
   -- * @LogRecord@ operations
   LogRecord (..),
   LogRecordArguments (..),
-  mkSeverityNumber,
-  shortName,
-  severityInt,
+  SeverityNumber (..),
+  toShortName,
   emitLogRecord,
 ) where
 
-import Control.Applicative
-import Control.Monad.Trans
-import Control.Monad.Trans.Maybe
-import Data.Coerce
-import qualified Data.HashMap.Strict as H
-import Data.IORef
-import Data.Maybe
-import GHC.IO (unsafePerformIO)
-import OpenTelemetry.Attributes (AttributeLimits)
-import OpenTelemetry.Common
-import OpenTelemetry.Context
-import OpenTelemetry.Context.ThreadLocal
 import OpenTelemetry.Internal.Common.Types
+import OpenTelemetry.Internal.Logging.Core
 import OpenTelemetry.Internal.Logging.Types
-import OpenTelemetry.Internal.Trace.Types
-import OpenTelemetry.LogAttributes
-import OpenTelemetry.Resource (MaterializedResources, emptyMaterializedResources)
-import System.Clock
 
-
-getCurrentTimestamp :: (MonadIO m) => m Timestamp
-getCurrentTimestamp = liftIO $ coerce @(IO TimeSpec) @(IO Timestamp) $ getTime Realtime
-
-
-data LoggerProviderOptions = LoggerProviderOptions
-  { loggerProviderOptionsResource :: MaterializedResources
-  , loggerProviderOptionsAttributeLimits :: AttributeLimits
-  }
-
-
-{- | Options for creating a @LoggerProvider@ with no resources and default limits.
-
- In effect, logging is a no-op when using this configuration.
--}
-emptyLoggerProviderOptions :: LoggerProviderOptions
-emptyLoggerProviderOptions =
-  LoggerProviderOptions
-    { loggerProviderOptionsResource = emptyMaterializedResources
-    }
-
-
-{- | Initialize a new @LoggerProvider@
-
- You should generally use @getGlobalLoggerProvider@ for most applications.
--}
-createLoggerProvider :: LoggerProviderOptions -> LoggerProvider
-createLoggerProvider LoggerProviderOptions {..} = LoggerProvider {loggerProviderResource = loggerProviderOptionsResource}
-
-
-globalLoggerProvider :: IORef LoggerProvider
-globalLoggerProvider = unsafePerformIO $ newIORef $ createLoggerProvider emptyLoggerProviderOptions
-{-# NOINLINE globalLoggerProvider #-}
-
-
--- | Access the globally configured @LoggerProvider@. This @LoggerProvider@ is no-op until initialized by the SDK
-getGlobalLoggerProvider :: (MonadIO m) => m LoggerProvider
-getGlobalLoggerProvider = liftIO $ readIORef globalLoggerProvider
-
-
-{- | Overwrite the globally configured @LoggerProvider@.
-
- @Logger@s acquired from the previously installed @LoggerProvider@s
- will continue to use that @LoggerProvider@s settings.
--}
-setGlobalLoggerProvider :: (MonadIO m) => LoggerProvider -> m ()
-setGlobalLoggerProvider = liftIO . writeIORef globalLoggerProvider
-
-
-makeLogger
-  :: LoggerProvider
-  -- ^ The @LoggerProvider@ holds the configuration for the @Logger@.
-  -> InstrumentationLibrary
-  -- ^ The library that the @Logger@ instruments. This uniquely identifies the @Logger@.
-  -> Logger
-makeLogger loggerProvider loggerInstrumentationScope = Logger {..}
-
-
-{- | Emits a LogRecord with properties specified by the passed in Logger and LogRecordArguments.
-If observedTimestamp is not set in LogRecordArguments, it will default to the current timestamp.
-If context is not specified in LogRecordArguments it will default to the current context.
--}
-emitLogRecord
-  :: (MonadIO m)
-  => Logger
-  -> LogRecordArguments body
-  -> m (LogRecord body)
-emitLogRecord Logger {..} LogRecordArguments {..} = do
-  currentTimestamp <- getCurrentTimestamp
-  let logRecordObservedTimestamp = fromMaybe currentTimestamp observedTimestamp
-
-  logRecordTracingDetails <- runMaybeT $ do
-    currentContext <- liftIO getContext
-    currentSpan <- MaybeT $ pure $ lookupSpan $ fromMaybe currentContext context
-    SpanContext {traceId, spanId, traceFlags} <- getSpanContext currentSpan
-    pure (traceId, spanId, traceFlags)
-
-  pure
-    LogRecord
-      { logRecordTimestamp = timestamp
-      , logRecordObservedTimestamp
-      , logRecordTracingDetails
-      , logRecordSeverityNumber = fmap mkSeverityNumber severityNumber
-      , logRecordSeverityText = severityText <|> (shortName . mkSeverityNumber =<< severityNumber)
-      , logRecordBody = body
-      , logRecordResource = loggerProviderResource loggerProvider
-      , logRecordInstrumentationScope = loggerInstrumentationScope
-      , logRecordAttributes =
-          addAttributes
-            (loggerProviderAttributeLimits loggerProvider)
-            emptyAttributes
-            attributes
-      }
