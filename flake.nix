3041a3f40272fe976f017962a57dda6f4086f38c
Giorgio Marinelli <marinelli@users.noreply.github.com>
2024-10-28 22:49:21 +0100

    Bump OTLP specification version to v1.0.0 (#150)
    * Bump OTLP
    
    * Fix building
    
    * Add generate-protocol.sh script
    
    * Update generate-protocol.sh script
    
    * Fix version
    
    * Improve generate-protocol.sh script
    
    * Improve generate-protocol.sh script
    
    * Improve generate-protocol.sh script
    
    * Switch to OTLP v1.0.0
    
    * Remove generate-protocol.sh script
    
    * Update flake.lock
    
    * Update otlp/package.yaml
    
    * Update ChangeLog.md and bump package version
    
    * Fix hs-opentelemetry-exporter-otlp constraints
    
    * Revert packages versions bumps and update otlp/ChangeLog.md
    
    * Revert hs-opentelemetry-otlp version bump

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     # };
 
     otlp-protobufs = {
-      url = "github:open-telemetry/opentelemetry-proto/b43e9b18b76abf3ee040164b55b9c355217151f3";
+      url = "github:open-telemetry/opentelemetry-proto/v1.0.0";
       flake = false;
     };
   };
