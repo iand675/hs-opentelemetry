7ba8a6d7e9d768fab557a9d2dc40b611461f7f52
Michael Peyton Jones <me@michaelpj.com>
2024-11-01 13:52:02 +0000

    Fix attribute length limits (#151)
    Fixes #113

--- sdk/test/Spec.hs
+++ sdk/test/Spec.hs
@@ -3,11 +3,14 @@ import qualified OpenTelemetry.ContextSpec as ContextSpec
 import qualified OpenTelemetry.ResourceSpec as ResourceSpec
 import OpenTelemetry.Trace (initializeGlobalTracerProvider)
 import qualified OpenTelemetry.TraceSpec as TraceSpec
+import System.Environment (setEnv)
 import Test.Hspec
 
 
 main :: IO ()
 main = do
+  -- For the attribute length limit test
+  setEnv "OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT" "50"
   initializeGlobalTracerProvider
   hspec $ do
     BaggageSpec.spec
