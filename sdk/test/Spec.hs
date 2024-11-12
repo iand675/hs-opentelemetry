24c408b933d20632cbc6413068c2a32e2b905ea6
ehofreiter <35861161+ehofreiter@users.noreply.github.com>
2024-11-12 05:15:13 -0600

    Fix resource merge (#156)
    Fixes #111

--- sdk/test/Spec.hs
+++ sdk/test/Spec.hs
@@ -11,6 +11,8 @@ main :: IO ()
 main = do
   -- For the attribute length limit test
   setEnv "OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT" "50"
+  -- For the resource attributes initialization test
+  setEnv "OTEL_RESOURCE_ATTRIBUTES" "host.name=env_host_name,example.name=env_example_name,example.count=42"
   initializeGlobalTracerProvider
   hspec $ do
     BaggageSpec.spec
