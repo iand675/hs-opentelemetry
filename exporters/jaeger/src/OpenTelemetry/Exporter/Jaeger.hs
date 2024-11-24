c75ce99a2a6cd4dd517440ca634a28534974aaa8
Giorgio Marinelli <marinelli@users.noreply.github.com>
2024-11-24 20:01:45 +0100

    Delete placeholder packages (#166)
    * Remove hs-opentelemetry-propagator-jaeger
    
    * Remove hs-opentelemetry-exporter-zipkin
    
    * Remove hs-opentelemetry-exporter-jaeger
    
    * Remove hs-opentelemetry-exporter-prometheus

--- exporters/jaeger/src/OpenTelemetry/Exporter/Jaeger.hs
+++
@@ -1,7 +0,0 @@
-module OpenTelemetry.Exporter.Jaeger
-  {-# DEPRECATED "use OpenTelemetry.Exporter.Jaeger.Span instead" #-} (
-  module OpenTelemetry.Exporter.Jaeger.Span,
-) where
-
-import OpenTelemetry.Exporter.Jaeger.Span
-
--- exporters/zipkin/src/OpenTelemetry/Exporter/Zipkin.hs
+++
@@ -1,7 +0,0 @@
-module OpenTelemetry.Exporter.Zipkin
-  {-# DEPRECATED "use OpenTelemetry.Exporter.Zipkin.Span instead" #-} (
-  module OpenTelemetry.Exporter.Zipkin.Span,
-) where
-
-import OpenTelemetry.Exporter.Zipkin.Span
-
