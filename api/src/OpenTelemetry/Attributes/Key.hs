5acf1d6be72db7270be989bac47c4d1bc9793f45
Kazuki Okamoto <kazuki.okamoto@herp.co.jp>
2024-11-26 15:13:12 +0900

    fix haddock comment
    Co-authored-by: ehofreiter <35861161+ehofreiter@users.noreply.github.com>

--- api/src/OpenTelemetry/Attributes/Key.hs
+++ api/src/OpenTelemetry/Attributes/Key.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE DerivingStrategies #-}
 
 {- |
-Module      :  OpenTelemetry.Attributes.Attribute
+Module      :  OpenTelemetry.Attributes.Key
 Copyright   :  (c) Ian Duncan, 2021
 License     :  BSD-3
 Description :  Key-value pair metadata used in 'OpenTelemetry.Trace.Span's, 'OpenTelemetry.Trace.Link's, and 'OpenTelemetry.Trace.Event's
