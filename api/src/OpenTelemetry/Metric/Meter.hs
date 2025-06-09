11dc9bb9a2f04beee7ff63f7e83086796d6c806f
Raphael Speyer <rspeyer@gmail.com>
2025-06-09 15:46:22 +0200

    allow the SDK implementations to be externalised

--- api/src/OpenTelemetry/Metric/Meter.hs
+++ api/src/OpenTelemetry/Metric/Meter.hs
@@ -2,11 +2,11 @@
 
 module OpenTelemetry.Metric.Meter (
   MeterProvider,
-  Meter,
+  getMeter,
   Version,
+  Meter,
   Description,
   UnitOfMeasure,
-  getMeter,
   createGauge,
   createObservableGauge,
   record,
@@ -16,40 +16,23 @@ module OpenTelemetry.Metric.Meter (
 import Data.Int (Int64)
 import Data.Text (Text)
 import OpenTelemetry.Attributes (AttributeMap)
-import System.Clock qualified as Clock
 
 
 data MeterProvider = MeterProvider
+  { getMeter :: Text -> Version -> IO Meter
+  }
 
 
 data Meter = Meter
   { scope :: Text
   , version :: Version
-  , getTime :: (() -> IO Clock.TimeSpec)
+  , mkSynchronousInstrument :: InstrumentKind -> Text -> Description -> UnitOfMeasure -> IO SynchronousInstrument
   }
 
 
 data Version
   = NoVersion
   | Version Text
-
-
-getMeter :: MeterProvider -> Text -> Version -> IO Meter
-getMeter _ scope version =
-  pure $
-    Meter
-      { scope = scope
-      , version = version
-      , getTime = getTime
-      }
-  where
-    getTime () = Clock.getTime (Clock.Monotonic)
-
-
-mkSynchronousInstrument :: Meter -> InstrumentKind -> Text -> Description -> UnitOfMeasure -> IO SynchronousInstrument
-mkSynchronousInstrument meter kind name desc unit =
-  -- TODO use the meter to remember this instrument so the exporters can find it
-  pure $ SynchronousInstrument meter kind name desc unit
 
 
 createGauge :: Meter -> Text -> Description -> UnitOfMeasure -> IO SynchronousInstrument
@@ -63,11 +46,11 @@ createObservableGauge meter name desc unit measure = do
 
 
 data SynchronousInstrument = SynchronousInstrument
-  { meter :: Meter
-  , kind :: InstrumentKind
+  { kind :: InstrumentKind
   , name :: Text
   , description :: Description
   , unit :: UnitOfMeasure
+  , record :: Int64 -> AttributeMap -> IO ()
   }
 
 
@@ -105,12 +88,6 @@ data Description
 data UnitOfMeasure
   = NoUnit
   | Unit Text
-
-
--- Records a measurement for a synchronous instrument.
--- Called by applications.
-record :: SynchronousInstrument -> Int64 -> AttributeMap -> IO ()
-record instr value attrs = pure () -- TODO: use the Meter put the value somewhere the exporters can get it
 
 
 -- Refreshes the value for the given asynchronous instrument
