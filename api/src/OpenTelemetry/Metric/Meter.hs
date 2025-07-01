677ec4baf4d46cb0f44207c5c2cb16306425ece7
Raphael Speyer <rspeyer@gmail.com>
2025-07-02 01:22:42 +0200

    review feedback

--- api/src/OpenTelemetry/Metric/Meter.hs
+++ api/src/OpenTelemetry/Metric/Meter.hs
@@ -1,12 +1,11 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
 module OpenTelemetry.Metric.Meter (
-  MeterProvider,
-  getMeter,
-  Version,
+  MeterProvider (getMeter),
+  Version (..),
   Meter,
-  Description,
-  UnitOfMeasure,
+  Description (..),
+  UnitOfMeasure (..),
   createGauge,
   createObservableGauge,
   createCounter,
@@ -24,11 +23,24 @@ import Data.Text (Text)
 import OpenTelemetry.Attributes (AttributeMap)
 
 
+{- | The entry point to the metrics API
+| See https://opentelemetry.io/docs/specs/otel/metrics/api/#meterprovider
+|
+| Example usage:
+|    meter <- getMeter meterProvider "home" NoVersion
+|    gauge <- createGauge meter "kettle.temp" (Description "The temperature of the water in the kettle") (Unit "Celcius")
+|    record guage 55 (H.singleton "room" kitchen))
+-}
 data MeterProvider = MeterProvider
   { getMeter :: Text -> Version -> IO Meter
+  -- ^ Generates a Meter
+  -- TODO add schema_url and attributes
   }
 
 
+{- | The Meter is responsible for creating Instruments, which allow us to record Measurements.
+| See https://opentelemetry.io/docs/specs/otel/metrics/api/#meter
+-}
 data Meter = Meter
   { scope :: Text
   , version :: Version
@@ -41,34 +53,42 @@ data Version
   | Version Text
 
 
+-- | https://opentelemetry.io/docs/specs/otel/metrics/api/#gauge-creation
 createGauge :: Meter -> Text -> Description -> UnitOfMeasure -> IO SynchronousInstrument
 createGauge meter = mkSynchronousInstrument meter GaugeKind
 
 
+-- | https://opentelemetry.io/docs/specs/otel/metrics/api/#asynchronous-gauge-creation
 createObservableGauge :: Meter -> Text -> Description -> UnitOfMeasure -> TakeMeasurements -> IO AsynchronousInstrument
 createObservableGauge meter name desc unit measure = AsynchronousInstrument measure <$> createGauge meter name desc unit
 
 
+-- | https://opentelemetry.io/docs/specs/otel/metrics/api/#counter-creation
 createCounter :: Meter -> Text -> Description -> UnitOfMeasure -> AggregationTemporality -> IO SynchronousInstrument
-createCounter meter name desc unit aggregation = mkSynchronousInstrument meter (SumKind OnlyUp aggregation) name desc unit
+createCounter meter name desc unit aggregation = mkSynchronousInstrument meter (SumKind (SumKindData OnlyUp aggregation)) name desc unit
 
 
+-- | https://opentelemetry.io/docs/specs/otel/metrics/api/#asynchronous-counter-creation
 createObservableCounter :: Meter -> Text -> Description -> UnitOfMeasure -> AggregationTemporality -> TakeMeasurements -> IO AsynchronousInstrument
 createObservableCounter meter name desc unit aggregation measure = AsynchronousInstrument measure <$> createCounter meter name desc unit aggregation
 
 
+-- | https://opentelemetry.io/docs/specs/otel/metrics/api/#updowncounter-creation
 createUpDownCounter :: Meter -> Text -> Description -> UnitOfMeasure -> AggregationTemporality -> IO SynchronousInstrument
-createUpDownCounter meter name desc unit aggregation = mkSynchronousInstrument meter (SumKind UpAndDown aggregation) name desc unit
+createUpDownCounter meter name desc unit aggregation = mkSynchronousInstrument meter (SumKind (SumKindData UpAndDown aggregation)) name desc unit
 
 
+-- | https://opentelemetry.io/docs/specs/otel/metrics/api/#asynchronous-updowncounter-creation
 createObservableUpDownCounter :: Meter -> Text -> Description -> UnitOfMeasure -> AggregationTemporality -> TakeMeasurements -> IO AsynchronousInstrument
 createObservableUpDownCounter meter name desc unit aggregation measure = AsynchronousInstrument measure <$> createUpDownCounter meter name desc unit aggregation
 
 
+-- | https://opentelemetry.io/docs/specs/otel/metrics/api/#histogram-creation
 createHistogram :: Meter -> Text -> Description -> UnitOfMeasure -> AggregationTemporality -> IO SynchronousInstrument
 createHistogram meter name desc unit aggregation = mkSynchronousInstrument meter (HistogramKind aggregation) name desc unit
 
 
+-- | https://opentelemetry.io/docs/specs/otel/metrics/api/#histogram-creation
 createObservableHistogram :: Meter -> Text -> Description -> UnitOfMeasure -> AggregationTemporality -> TakeMeasurements -> IO AsynchronousInstrument
 createObservableHistogram meter name desc unit aggregation measure = AsynchronousInstrument measure <$> createHistogram meter name desc unit aggregation
 
@@ -93,11 +113,14 @@ data AsynchronousInstrument = AsynchronousInstrument
 
 data InstrumentKind
   = GaugeKind
-  | SumKind
-      { monotonic :: Monotonicity
-      , aggregation :: AggregationTemporality
-      }
-  | HistogramKind {aggregation :: AggregationTemporality}
+  | SumKind SumKindData
+  | HistogramKind AggregationTemporality
+
+
+data SumKindData = SumKindData
+  { monotonic :: Monotonicity
+  , aggregation :: AggregationTemporality
+  }
 
 
 -- Whether each measurement is the newest cumulative value, or a delta to be added to previous results
