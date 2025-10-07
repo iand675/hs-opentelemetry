4ceb48edda791fd944a1ffe8ad18e96ec661f624
Wen Kokke <wenkokke@users.noreply.github.com>
2025-10-07 14:12:00 +0100

    Refactor SpanExporter API

--- api/src/OpenTelemetry/Common.hs
+++ api/src/OpenTelemetry/Common.hs
@@ -1,11 +1,16 @@
 module OpenTelemetry.Common where
 
-import Data.Word (Word8)
-import System.Clock (TimeSpec)
+import Data.Word (Word64, Word8)
+import System.Clock (TimeSpec, toNanoSecs)
 
 
 newtype Timestamp = Timestamp TimeSpec
   deriving (Read, Show, Eq, Ord)
+
+
+-- | Convert a 'Timestamp' to a 'Word64' in nanoseconds.
+timestampToNano :: Timestamp -> Word64
+timestampToNano (Timestamp t) = fromIntegral (toNanoSecs t)
 
 
 -- | Contain details about the trace. Unlike TraceState values, TraceFlags are present in all traces. The current version of the specification only supports a single flag called sampled.
