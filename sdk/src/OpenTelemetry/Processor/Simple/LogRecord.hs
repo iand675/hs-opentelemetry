70afe05e05e587ed269d03b279745a7c2a4f95d8
Ian Duncan <ian@iankduncan.com>
2026-05-18 21:23:51 +0200

    feat(sdk): batch and simple LogRecordProcessor (#247)

--- sdk/src/OpenTelemetry/Processor/Simple/LogRecord.hs
+++ sdk/src/OpenTelemetry/Processor/Simple/LogRecord.hs
@@ -1,2 +1,101 @@
-module OpenTelemetry.Processor.Simple.LogRecord () where
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
+module OpenTelemetry.Processor.Simple.LogRecord (
+  SimpleLogRecordProcessorConfig (..),
+  simpleLogRecordProcessor,
+) where
+
+import Control.Concurrent.Async
+import Control.Concurrent.STM
+import Control.Monad
+import Data.IORef
+import qualified Data.Vector as V
+import OpenTelemetry.Internal.Common.Types (ShutdownResult (..))
+import OpenTelemetry.Internal.Logs.Types
+import System.IO (hPutStrLn, stderr)
+
+
+data SimpleLogRecordProcessorConfig = SimpleLogRecordProcessorConfig
+  { simpleLogRecordExporter :: LogRecordExporter
+  , simpleLogRecordExportTimeoutMicros :: !Int
+  -- ^ Timeout for individual export calls in microseconds. Default: 30s.
+  }
+
+
+defaultSimpleQueueBound :: Int
+defaultSimpleQueueBound = 2048
+
+
+simpleLogRecordProcessor :: SimpleLogRecordProcessorConfig -> IO LogRecordProcessor
+simpleLogRecordProcessor SimpleLogRecordProcessorConfig {..} = do
+  queue <- newTBQueueIO (fromIntegral defaultSimpleQueueBound)
+  droppedRef <- newIORef (0 :: Int)
+  warnedRef <- newIORef False
+  shutdownVar <- newTVarIO False
+  flushReq <- newEmptyTMVarIO
+  flushDone <- newEmptyTMVarIO
+
+  let exportOne rw = do
+        let readable = mkReadableLogRecord rw
+        mask_ (logRecordExporterExport simpleLogRecordExporter (V.singleton readable))
+
+  let drainQueue = do
+        mRw <- atomically $ tryReadTBQueue queue
+        case mRw of
+          Nothing -> pure ()
+          Just rw -> do
+            _ <- exportOne rw
+            drainQueue
+
+  let workerLoop = do
+        cmd <-
+          atomically $
+            msum
+              [ Nothing <$ (readTVar shutdownVar >>= check)
+              , Just Nothing <$ takeTMVar flushReq
+              , Just . Just <$> readTBQueue queue
+              ]
+        case cmd of
+          Nothing -> do
+            drainQueue
+            void $ atomically $ tryPutTMVar flushDone ()
+          Just Nothing -> do
+            drainQueue
+            atomically $ putTMVar flushDone ()
+            workerLoop
+          Just (Just rw) -> do
+            _ <- exportOne rw
+            workerLoop
+
+  exportWorker <- async workerLoop
+
+  pure
+    LogRecordProcessor
+      { logRecordProcessorOnEmit = \lr _ctxt -> do
+          written <- atomically $ tryWriteTBQueue queue lr
+          unless written $ do
+            n <- atomicModifyIORef' droppedRef (\c -> let c' = c + 1 in (c', c'))
+            alreadyWarned <- atomicModifyIORef' warnedRef (\w -> (True, w))
+            -- TODO: otel-12 introduces OpenTelemetry.Internal.Logging (otelLogWarning)
+            -- which respects OTEL_LOG_LEVEL and the global error handler.
+            -- Replace hPutStrLn stderr with otelLogWarning once available.
+            unless alreadyWarned $
+              hPutStrLn stderr $
+                "OpenTelemetry [WARN] SimpleLogRecordProcessor: queue full (capacity "
+                  <> show defaultSimpleQueueBound
+                  <> "), dropping log record. Total dropped so far: "
+                  <> show n
+      , logRecordProcessorShutdown = do
+          atomically $ writeTVar shutdownVar True
+          async $ do
+            wait exportWorker
+            logRecordExporterShutdown simpleLogRecordExporter
+            pure ShutdownSuccess
+      , logRecordProcessorForceFlush = do
+          isShut <- readTVarIO shutdownVar
+          unless isShut $ do
+            atomically $ putTMVar flushReq ()
+            atomically $ takeTMVar flushDone
+          logRecordExporterForceFlush simpleLogRecordExporter
+      }
