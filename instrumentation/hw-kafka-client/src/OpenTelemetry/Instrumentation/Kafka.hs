86a79b5c348c4aaed5d9aaa151f732f85fafc690
Ian Duncan <ian@iankduncan.com>
2026-05-20 23:34:00 +0200

    ian/instrumentation-hw-kafka: update for API 0.4

--- instrumentation/hw-kafka-client/src/OpenTelemetry/Instrumentation/Kafka.hs
+++ instrumentation/hw-kafka-client/src/OpenTelemetry/Instrumentation/Kafka.hs
@@ -17,21 +17,26 @@ module OpenTelemetry.Instrumentation.Kafka (
 
   -- * Consumer
   pollMessage,
+
+  -- * Attribute builders (exported for testing)
+  producerAttributes,
+  consumerAttributes,
 ) where
 
-import Control.Monad (void)
-import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.IO.Unlift (MonadUnliftIO)
-import Data.Bifunctor (first)
 import Data.ByteString (ByteString)
-import qualified Data.CaseInsensitive as CI
+import qualified Data.ByteString as BS
+import Data.Int (Int64)
 import qualified Data.Map as M
 import Data.String (IsString)
+import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8')
+import qualified Data.Text.Encoding as TE
 import GHC.Stack.Types (HasCallStack)
 import Kafka.Consumer (
   ConsumerProperties (cpProps),
-  ConsumerRecord (crHeaders, crKey, crOffset, crPartition, crTopic),
+  ConsumerRecord (crHeaders, crKey, crOffset, crPartition, crTopic, crValue),
   KafkaConsumer,
   Offset (unOffset),
  )
@@ -40,7 +45,7 @@ import Kafka.Producer (
   KafkaError,
   KafkaProducer,
   ProducePartition (SpecifiedPartition, UnassignedPartition),
-  ProducerRecord (prHeaders, prKey, prPartition, prTopic),
+  ProducerRecord (prHeaders, prKey, prPartition, prTopic, prValue),
  )
 import qualified Kafka.Producer as KP
 import Kafka.Types (
@@ -51,31 +56,40 @@ import Kafka.Types (
   headersFromList,
   headersToList,
  )
-import Network.HTTP.Types (RequestHeaders)
+import OpenTelemetry.Attributes.Key (unkey)
 import OpenTelemetry.Attributes.Map (AttributeMap, insertAttributeByKey)
 import qualified OpenTelemetry.Context as Context
 import OpenTelemetry.Context.ThreadLocal (attachContext, getContext)
-import OpenTelemetry.Propagator (extract, inject)
+import OpenTelemetry.Propagator (TextMap, emptyTextMap, extract, getGlobalTextMapPropagator, inject, textMapFromList, textMapToList)
 import OpenTelemetry.SemanticConventions (
+  error_type,
+  messaging_client_id,
+  messaging_consumer_group_name,
   messaging_destination_name,
   messaging_kafka_consumer_group,
   messaging_kafka_destination_partition,
   messaging_kafka_message_key,
   messaging_kafka_message_offset,
+  messaging_message_body_size,
   messaging_operation,
+  messaging_operation_name,
+  messaging_operation_type,
+  messaging_system,
  )
 import OpenTelemetry.Trace.Core (
   SpanArguments (kind),
   SpanKind (Consumer, Producer),
+  SpanStatus (Error),
   Tracer,
+  addAttribute,
   addAttributesToSpanArguments,
   callerAttributes,
   defaultSpanArguments,
   detectInstrumentationLibrary,
   getGlobalTracerProvider,
-  getTracerProviderPropagators,
   inSpan'',
   makeTracer,
+  setStatus,
   toAttribute,
   tracerOptions,
  )
@@ -100,12 +114,16 @@ rightToMaybe (Right b) = Just b
 rightToMaybe _ = Nothing
 
 
--- | Span attributes with caller information and kafka specifics
 producerAttributes :: HasCallStack => ProducerRecord -> AttributeMap
 producerAttributes record =
   let
+    addSystem =
+      insertAttributeByKey messaging_system (toAttribute ("kafka" :: T.Text))
     addOperationName =
       insertAttributeByKey messaging_operation producerOperationName
+        . insertAttributeByKey messaging_operation_name (toAttribute (producerOperationName :: T.Text))
+    addOperationType =
+      insertAttributeByKey messaging_operation_type (toAttribute ("send" :: T.Text))
     addDestination =
       insertAttributeByKey messaging_destination_name $ toAttribute . unTopicName . prTopic $ record
     addPartition =
@@ -120,8 +138,12 @@ producerAttributes record =
           insertAttributeByKey messaging_kafka_message_key $ toAttribute key
         Nothing ->
           id
+    addBodySize =
+      case prValue record of
+        Just v -> insertAttributeByKey messaging_message_body_size (toAttribute (fromIntegral (BS.length v) :: Int64))
+        Nothing -> id
   in
-    (addOperationName . addDestination . addPartition . addKey)
+    (addSystem . addOperationName . addOperationType . addDestination . addPartition . addKey . addBodySize)
       callerAttributes
 
 
@@ -130,7 +152,6 @@ consumerSpanArgs :: SpanArguments
 consumerSpanArgs = defaultSpanArguments {kind = Consumer}
 
 
--- | Span attributes for consumer with caller information and kafka specifics
 consumerAttributes
   :: HasCallStack
   => ConsumerProperties
@@ -138,14 +159,24 @@ consumerAttributes
   -> AttributeMap
 consumerAttributes consumerProperties record =
   let
+    addSystem =
+      insertAttributeByKey messaging_system (toAttribute ("kafka" :: T.Text))
     addOperationName =
       insertAttributeByKey messaging_operation consumerOperationName
+        . insertAttributeByKey messaging_operation_name (toAttribute (consumerOperationName :: T.Text))
+    addOperationType =
+      insertAttributeByKey messaging_operation_type (toAttribute ("process" :: T.Text))
     addDestination =
       insertAttributeByKey messaging_destination_name $ toAttribute . unTopicName . crTopic $ record
     addConsumerGroup =
-      -- NOTE: unfortunately, hw-kafka-client does not expose an API to get the consumer, this a flaky workaround
       case M.lookup "group.id" $ cpProps consumerProperties of
-        Just groupId -> insertAttributeByKey messaging_kafka_consumer_group $ toAttribute groupId
+        Just groupId ->
+          insertAttributeByKey messaging_kafka_consumer_group (toAttribute groupId)
+            . insertAttributeByKey messaging_consumer_group_name (toAttribute groupId)
+        Nothing -> id
+    addClientId =
+      case M.lookup "client.id" $ cpProps consumerProperties of
+        Just cid -> insertAttributeByKey messaging_client_id (toAttribute cid)
         Nothing -> id
     addPartition =
       insertAttributeByKey messaging_kafka_destination_partition $ toAttribute . unPartitionId . crPartition $ record
@@ -157,8 +188,22 @@ consumerAttributes consumerProperties record =
           insertAttributeByKey messaging_kafka_message_key $ toAttribute key
         Nothing ->
           id
+    addBodySize =
+      case crValue record of
+        Just v -> insertAttributeByKey messaging_message_body_size (toAttribute (fromIntegral (BS.length v) :: Int64))
+        Nothing -> id
   in
-    (addOperationName . addDestination . addConsumerGroup . addPartition . addOffset . addKey)
+    ( addSystem
+        . addOperationName
+        . addOperationType
+        . addDestination
+        . addConsumerGroup
+        . addClientId
+        . addPartition
+        . addOffset
+        . addKey
+        . addBodySize
+    )
       callerAttributes
 
 
@@ -169,14 +214,12 @@ rdkafkaTracer = do
   return $ makeTracer provider $detectInstrumentationLibrary tracerOptions
 
 
--- | Convert Kafka headers to HTTP headers format
-kafkaHeadersToHttpHeaders :: Headers -> RequestHeaders
-kafkaHeadersToHttpHeaders = map (first CI.mk) . headersToList
+kafkaHeadersToTextMap :: Headers -> TextMap
+kafkaHeadersToTextMap = textMapFromList . map (\(k, v) -> (TE.decodeUtf8 k, TE.decodeUtf8 v)) . headersToList
 
 
--- | Convert HTTP headers to Kafka headers format
-httpHeadersToKafkaHeaders :: RequestHeaders -> Headers
-httpHeadersToKafkaHeaders = headersFromList . map (first CI.foldedCase)
+textMapToKafkaHeaders :: TextMap -> Headers
+textMapToKafkaHeaders = headersFromList . map (\(k, v) -> (TE.encodeUtf8 k, TE.encodeUtf8 v)) . textMapToList
 
 
 {- | Produce a message to Kafka with OpenTelemetry instrumentation.
@@ -202,11 +245,18 @@ produceMessage producer record =
       tracer <- rdkafkaTracer
       ctxt <- getContext
       inSpan'' tracer spanName spanArguments $ \newSpan -> do
-        propagator <- getTracerProviderPropagators <$> getGlobalTracerProvider
-        extraHeaders <- inject propagator (Context.insertSpan newSpan ctxt) []
-        let newKafkaHeaders = headers <> httpHeadersToKafkaHeaders extraHeaders
+        propagator <- liftIO getGlobalTextMapPropagator
+        extraTm <- inject propagator (Context.insertSpan newSpan ctxt) emptyTextMap
+        let newKafkaHeaders = headers <> textMapToKafkaHeaders extraTm
         let newKafkaRecord = record {prHeaders = newKafkaHeaders}
-        KP.produceMessage producer newKafkaRecord
+        result <- KP.produceMessage producer newKafkaRecord
+        case result of
+          Just err -> do
+            let errText = T.pack $ show err
+            addAttribute newSpan (unkey error_type) errText
+            setStatus newSpan (Error errText)
+          Nothing -> pure ()
+        pure result
 
 
 {- | Poll for a single message from Kafka with OpenTelemetry instrumentation.
@@ -235,8 +285,8 @@ pollMessage consumerProperties consumer timeout =
           do
             tracer <- rdkafkaTracer
             ctxt <- getContext
-            propagator <- getTracerProviderPropagators <$> getGlobalTracerProvider
-            ctx <- extract propagator (kafkaHeadersToHttpHeaders $ crHeaders cr) ctxt
-            void $ attachContext ctx
+            propagator <- liftIO getGlobalTextMapPropagator
+            ctx <- extract propagator (kafkaHeadersToTextMap $ crHeaders cr) ctxt
+            _ <- attachContext ctx
             inSpan'' tracer spanName (addAttributesToSpanArguments attributes consumerSpanArgs) $ \_span -> do
               return $ Right cr
